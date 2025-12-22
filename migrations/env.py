9a0c3ec57e57183a8e6fbd8bdf77fcc729f3ffbc
Denys Pryslipskyi <denys@Denyss-MacBook-Air.local>
2025-12-22 17:51:40 +0100

    add model made migration changed db conf

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,10 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from src.core.config import get_settings
 from src.core.database import Base  # noqa: F403
+import src.models  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +29,10 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_url() -> str:
+    settings = get_settings()
+    return settings.DATABASE_URL.unicode_string().replace("postgresql+asyncpg", "postgresql+psycopg")
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -39,9 +46,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=get_url(),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -58,15 +64,18 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, compare_type=True
         )
 
         with context.begin_transaction():
